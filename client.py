70326c6fdfc1eab0a73476736cf50031e244926d
Nuno Ribeiro <nribeiro@semtech.com>
2022-02-28 15:48:26 +0100

    Fixes IP

--- client.py
+++ client.py
@@ -6,7 +6,7 @@ import ssl
 async def main():
     sslcontext = ssl.create_default_context(purpose=ssl.Purpose.SERVER_AUTH, cafile='example.crt')
     async with aiohttp.ClientSession() as session:
-        async with session.get("https://0.0.0.0:8443/JOHNYY", ssl=sslcontext) as response:
+        async with session.get("https://127.0.0.1:8443/JOHNYY", ssl=sslcontext) as response:
             html = await response.read()
             print(html)
 
